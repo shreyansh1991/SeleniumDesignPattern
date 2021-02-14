6bec6af53481a128f88e911723c039e7283ae699
Shreyansh Jain <shreyansh791@yahoo.in>
2021-02-14 11:56:25 +0530

    code refactor

---
+++ src/main/java/com/udemy/seleniumdesign/singleton/Singleton.java
@@ -0,0 +1,23 @@
+package com.udemy.seleniumdesign.singleton;
+public class Singleton {
+
+    //private constructor
+    private Singleton(){
+
+    }
+    /*
+    inner static class SingletonHelper is loaded when
+    SingletonHelper.theSingletonInstance is executed/called.
+     */
+    private static class SingletonHelper {
+        private static final Singleton theSingletonInstance = new Singleton();
+    }
+    //public static method to get theSingletonInstance
+    public static Singleton getInstance(){
+        return SingletonHelper.theSingletonInstance;
+        /*
+        when above line is executed SingletonHelper class is loaded and
+        then static variable theSingletonInstance is initialized
+         */
+    }
+}
